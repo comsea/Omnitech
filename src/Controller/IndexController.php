16eb019cdfa19a9a636fc3ed5d90fecc380c0bde
Aurore Bartel <a.bartel@comsea.fr>
2023-05-17 16:57:48 +0200

    affichage news

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\News;
+use App\Repository\NewsRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
-    #[Route('/', name: 'app_index')]
-    public function index(): Response
+    private NewsRepository $newsrepository;
+
+    public function __construct(NewsRepository $newsrepository)
     {
+        $this->newsrepository = $newsrepository;
+    }
+
+    #[Route('/', name: 'app_index')]
+    public function index(ManagerRegistry $doctrine): Response
+    {
+        $news = $this->newsrepository->findAll();
+
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
+            "news" => $news
         ]);
     }
 }

d1ccfae48fde799042b723a211c55807c31683a1
Aurore Bartel <a.bartel@comsea.fr>
2023-06-07 10:14:36 +0200

    début responsive

--- assets/app.js
+++ assets/app.js
@@ -15,6 +15,35 @@ $(document).ready(function(){
         slidesToShow: 3,
         slidesToScroll: 3,
         dots: true,
-        arrows: true
+        arrows: true,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true,
+              arrows: false
+            }
+    }]
       });
   });
+
+// Navbar
+const toggleButton = document.getElementById('toggle-button');
+const navList = document.getElementById('nav');
+
+const hide = () => {
+  navList.classList.remove('active');
+}
+
+toggleButton.addEventListener('click', event => {
+  event.stopPropagation()
+  navList.classList.toggle('active');
+})
+
+const handleClosure = event => !navList.contains(event.target) && hide()
+
+window.addEventListener('click', handleClosure)
+window.addEventListener('focusin', handleClosure)
